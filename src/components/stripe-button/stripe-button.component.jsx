dee2004810114758a844de5dfaaad7d7591b43e5
Geetansh <gitansh49@gmail.com>
2020-08-06 04:13:33 +0530

    final app ready for production

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51HCtcmJZKiEMVEimAZt0nQ5qzrR2ve6mvO0qECWoxVF5KU7zLX1LwoAwoG74KhzB3EIfIEgELibY37gVypNOfobF001kvGSdYa";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Ecomm App"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
